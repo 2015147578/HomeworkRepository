b7e347aea235e4f54ed6862fb8716ad21be0d0c3
2015147578 <hayan765@gmail.com>
2022-05-31 21:58:26 +0900

    LAB5

--- LAB5/script.js
+++ LAB5/script.js
@@ -11,43 +11,43 @@ fetch(myReq).then(response => response.json()).then(
 
 function init(product) {
 	const fprice = document.getElementById('filterprice')
-    const fname = document.getElementById('filtername');
-    const fbtn = document.getElementById('filterbtn');
+	const fname = document.getElementById('filtername');
+	const fbtn = document.getElementById('filterbtn');
 	const pdbox = document.getElementById('pdbox');
     
 	let currPrice = fprice.value;
 	let currName = '';
 	
 	let priceFiltered;	// 가격으로 필터링
-    let nameFiltered;	// 이름으로 필터링
+	let nameFiltered;	// 이름으로 필터링
 
-    nameFiltered = product;
-    productUpdate();
+	nameFiltered = product;
+	productUpdate();
 
-    priceFiltered = [];
-    nameFiltered = [];
+	priceFiltered = [];
+	nameFiltered = [];
 
-    fbtn.onclick = filterPrice;
+	fbtn.onclick = filterPrice;
 
-    function filterPrice(e) {
-        e.preventDefault();
+	function filterPrice(e) {
+		e.preventDefault();
 
-        priceFiltered = [];
-        nameFiltered = [];
+		priceFiltered = [];
+		nameFiltered = [];
 
-        if(fprice.value === currPrice && fname.value.trim() === currName) {
-            return;
-        }
+		if(fprice.value === currPrice && fname.value.trim() === currName) {
+			return;
+		}
 		else {
-            currPrice = fprice.value;
-            currName = fname.value.trim();
+			currPrice = fprice.value;
+			currName = fname.value.trim();
 
-            if(fprice.value === 'all') {
-                priceFiltered = product;
-                filterName();
-            }
+			if(fprice.value === 'all') {
+				priceFiltered = product;
+				filterName();
+			}
 			else {
-                let price = fprice.value;
+				let price = fprice.value;
 				if (price === "5000") {					// 가격 5000 이하
 					for(let i = 0; i < product.length; i++){
 						if(product[i].price <= 5000) {
@@ -76,53 +76,53 @@ function init(product) {
 						}
 					}
 				}
-                filterName();
-            }
-        }
-    }
+				filterName();
+			}
+		}
+	}
 
-    function filterName() {
-        if(fname.value.trim() === '') {		// 검색어 없음
-            nameFiltered = priceFiltered;
-            productUpdate();
-        }
+	function filterName() {
+		if(fname.value.trim() === '') {		// 검색어 없음
+			nameFiltered = priceFiltered;
+			productUpdate();
+		}
 		else {
-            let nametosearch = fname.value.trim().toLowerCase();
+			let nametosearch = fname.value.trim().toLowerCase();
     
-            for(let i = 0; i < priceFiltered.length; i++) {
-                if(priceFiltered[i].name.toLowerCase().includes(nametosearch)) {
-                    nameFiltered.push(priceFiltered[i]);
-                }
-            }
-            productUpdate();
-        }
-    }
+			for(let i = 0; i < priceFiltered.length; i++) {
+				if(priceFiltered[i].name.toLowerCase().includes(nametosearch)) {
+					nameFiltered.push(priceFiltered[i]);
+				}
+			}
+			productUpdate();
+		}
+	}
     
-    function productUpdate() {
-        while (pdbox.firstChild) {
-            pdbox.removeChild(pdbox.firstChild);
-        }
+	function productUpdate() {
+		while (pdbox.firstChild) {
+			pdbox.removeChild(pdbox.firstChild);
+		}
     
-        if(nameFiltered.length === 0) {
-            const nresult = document.createElement('p');
-            nresult.textContent = 'No Result';
-            pdbox.appendChild(nresult);
-        }
+		if(nameFiltered.length === 0) {
+			const nresult = document.createElement('p');
+			nresult.textContent = 'No Result';
+			pdbox.appendChild(nresult);
+		}
 		else {
 			imgcounter = 4;
 			globpd = nameFiltered;
-            for(let i = 0; i < 4; i++) {
-                createImgUrl(nameFiltered[i], i);
-            }
-        }
-    }
+			for(let i = 0; i < 4; i++) {
+				createImgUrl(nameFiltered[i], i);
+			}
+		}
+	}
     
-    function createImgUrl(product, i) {
-        let url = './' + product.src;
-        showProduct(url, product, i);
-    }
+	function createImgUrl(product, i) {
+		let url = './' + product.src;
+		showProduct(url, product, i);
+	}
     
-    function showProduct(imgurl, product, i) {		
+	function showProduct(imgurl, product, i) {		
 		const outerdiv = document.createElement('div');
 		const img = document.createElement('img');
 		
@@ -163,22 +163,22 @@ function init(product) {
 }
 
 window.addEventListener('scroll', () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-    if(clientHeight + scrollTop >= scrollHeight - 10) {
-        infscroll();
-    }
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	if(clientHeight + scrollTop >= scrollHeight - 10) {
+		infscroll();
+	}
 });
 
 function infscroll() {
-    const pdbox = document.getElementById('pdbox');
+	const pdbox = document.getElementById('pdbox');
 
-    var start = imgcount;
-    var end = start + 2;
-    imgcount = end;
+	var start = imgcount;
+	var end = start + 2;
+	imgcount = end;
 	
-    let product = globpd;
-    for(start; start < end; start++) {
-        const outerdiv = document.createElement('div');
+	let product = globpd;
+	for(start; start < end; start++) {
+		const outerdiv = document.createElement('div');
 		const img = document.createElement('img');
 		
 		const innerdiv = document.createElement('div');
@@ -186,17 +186,17 @@ function infscroll() {
 		
 		const click = document.createElement('p');
 
-        let url = './' + product[start].src;
+		let url = './' + product[start].src;
     
-        outerdiv.setAttribute('class', 'pdimg');
+		outerdiv.setAttribute('class', 'pdimg');
 			
 		img.src = url;
 		img.alt = product.name;
 
 		innerdiv.setAttribute('class', 'pdinfo');
-        innerdiv.id = start;
-        innerdiv.style.opacity = "0";
-        innerdiv.onclick =
+		innerdiv.id = start;
+		innerdiv.style.opacity = "0";
+		innerdiv.onclick =
 			function() {
 				let div =  document.getElementById(this.id);
 				if (div.style.opacity === "0") {
@@ -207,14 +207,14 @@ function infscroll() {
 				}
 			}
 
-        pinfo.innerHTML = 'Product : ' + product[start].name + '<br/>Price(₩) : ' + product[start].price;
+		pinfo.innerHTML = 'Product : ' + product[start].name + '<br/>Price(₩) : ' + product[start].price;
 			
 		click.textContent = "Click for detail";
     
-        pdbox.appendChild(outerdiv);
+		pdbox.appendChild(outerdiv);
 		outerdiv.appendChild(innerdiv);
 		innerdiv.appendChild(pinfo);
 		outerdiv.appendChild(img);
 		outerdiv.appendChild(click);
-    }
+	}
 }
